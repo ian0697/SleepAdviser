347034968a7e05b6a0191ac7176f475d3014f060
Ian Santiago <iansantiago0697@gmail.com>
2017-09-08 00:12:14 +0800

    Commit change test

--- app/src/main/java/com/santiagoapps/sleepadviser/LoginActivity.java
+++ app/src/main/java/com/santiagoapps/sleepadviser/LoginActivity.java
@@ -61,7 +61,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-    //some changes
+    //some changes hehe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
