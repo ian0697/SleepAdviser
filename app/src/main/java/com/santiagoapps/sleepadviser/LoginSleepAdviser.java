f43dff1dac117cc8fa09142167aae8c4cc963313
Ian Santiago <iansantiago0697@gmail.com>
2017-09-08 01:26:18 +0800

    Deleted first activity

---
+++ app/src/main/java/com/santiagoapps/sleepadviser/LoginSleepAdviser.java
@@ -0,0 +1,13 @@
+package com.santiagoapps.sleepadviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSleepAdviser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sleep_adviser);
+    }
+}
